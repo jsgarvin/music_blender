a1db516c0ba56e2d14d17be542073c82d71bb2ac
Jonathan Garvin <jon@5valleys.com>
2013-05-31 14:18:34 -0600

    rename lib folder

--- lib/mmp.rb
+++ lib/mmp.rb
@@ -1,3 +1,3 @@
 require 'bundler/setup'
-require File.expand_path('../mmp/configuration', __FILE__)
-require File.expand_path('../mmp/shell', __FILE__)
+require File.expand_path('../my_media_player/configuration', __FILE__)
+require File.expand_path('../my_media_player/shell', __FILE__)
