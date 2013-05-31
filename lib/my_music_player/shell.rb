ba5980228b865cef1f82347c1339558e15f66f55
Jonathan Garvin <jon@5valleys.com>
2013-05-31 14:36:57 -0600

    imporove and fix shell test

--- lib/my_music_player/shell.rb
+++ lib/my_music_player/shell.rb
@@ -9,19 +9,15 @@ module MyMusicPlayer
       until command.to_s == 'exit'
         print 'mmp> '
         command = commands.shift || gets.strip
-        if self.respond_to?("mmp_#{command}")
-          send("mmp_#{command}")
+        if self.respond_to?("_mmp_#{command}")
+          send("_mmp_#{command}")
         else
           puts "Unrecognized Command: #{command}"
         end
       end 
     end
 
-    #######
-    private
-    #######
-
-    def mmp_exit
+    def _mmp_exit
       puts "Exiting..."
     end
 
--- test/unit/shell_test.rb
+++ test/unit/shell_test.rb
@@ -4,9 +4,10 @@ module MyMusicPlayer
   class ShellTest < MiniTest::Unit::TestCase
 
     def test_execute_a_command_and_exit
-      Shell.instance.expects(:mmp_ping)
-      Shell.instance.expects(:mmp_exit)
+      Shell.instance.expects(:_mmp_ping)
+      assert_equal('',$stdout.string)
       Shell.instance.run(:ping)
+      assert_match(/Exiting/,$stdout.string)
     end
 
     def test_gracefully_fail_to_execute_command
