a20f4dfb1abd4568b76467a00347d38f48a9f147
Jonathan Garvin <jon@5valleys.com>
2013-06-16 21:01:04 -0600

    update scanner test

--- test/unit/scanner_test.rb
+++ test/unit/scanner_test.rb
@@ -4,7 +4,7 @@ module MyMusicPlayer
   class ScannerTest < MiniTest::Unit::TestCase
 
     def test_should_find_files_in_music_folder
-      puts Scanner.instance.ls.inspect
+      assert Scanner.instance.ls.include?(File.expand_path('../../music/point1sec.mp3', __FILE__))
     end
     
   end
