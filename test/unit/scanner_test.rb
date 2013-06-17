ae5259b8e0ea6e20c358479dec0f56db0c927799
Jonathan Garvin <jon@5valleys.com>
2013-06-16 21:30:02 -0600

    make ls return only relative paths

--- test/unit/scanner_test.rb
+++ test/unit/scanner_test.rb
@@ -4,7 +4,8 @@ module MyMusicPlayer
   class ScannerTest < MiniTest::Unit::TestCase
 
     def test_should_find_files_in_music_folder
-      assert Scanner.instance.ls.include?(File.expand_path('../../music/point1sec.mp3', __FILE__))
+      assert Scanner.instance.ls.include?('point1sec.mp3')
+      assert Scanner.instance.ls.include?('subfolder/insubfolder.mp3')
     end
     
   end
