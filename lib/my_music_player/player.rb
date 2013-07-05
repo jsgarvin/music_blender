c61b63e22827fd46af1627667ef6498f73129bb1
Jonathan Garvin <jon@5valleys.com>
2013-07-04 21:22:57 -0600

    playing music from the command line

---
+++ lib/my_music_player/player.rb
@@ -0,0 +1,39 @@
+module MyMusicPlayer
+  class Player
+    include Singleton
+
+    attr_reader :music_thread
+
+    def play
+      @music_thread ||= Thread.new do
+        play_music_until_stopped
+      end      
+    end
+    
+    def stop
+      @music_thread.exit if playing?
+    end
+
+    #######
+    private
+    #######
+    
+    def playing?
+      @music_thread and @music_thread.alive?
+    end
+
+    def play_music_until_stopped
+      while true
+        play_next_song
+      end
+    end
+
+    def play_next_song
+      `mpg321 '#{pick_a_song}'`
+    end
+
+    def pick_a_song
+      Scanner.instance.ls.sample
+    end
+  end
+end
