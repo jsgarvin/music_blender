f0266f46ad6f371977798ab4c852e2bd422d596e
Jonathan Garvin <jon@5valleys.com>
2013-07-15 16:53:37 -0600

    require open3

--- lib/my_music_player.rb
+++ lib/my_music_player.rb
@@ -1,6 +1,7 @@
 require 'bundler/setup'
 require 'find'
 require 'singleton'
+require 'open3'
 require File.expand_path('../my_music_player/configuration', __FILE__)
 require File.expand_path('../my_music_player/player', __FILE__)
 require File.expand_path('../my_music_player/scanner', __FILE__)
