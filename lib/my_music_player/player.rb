df2b52890ca1cce6012f10fe3b3e682270b3c468
Jonathan Garvin <jon@5valleys.com>
2013-08-17 17:51:26 -0600

    playing songs one after another

--- lib/my_music_player/player.rb
+++ lib/my_music_player/player.rb
@@ -5,28 +5,38 @@ module MyMusicPlayer
     attr_reader :stdin, :stdout, :stderr, :thread
 
     def initialize
-      @stdin, @stdout, @stderr, @thread = Open3.popen3('mpg321 -R meaningless_but_required_bogus_argument')
+      @stdin, @stdout, @stderr, @music_thread = Open3.popen3('mpg321 -R meaningless_but_required_bogus_argument')
     end
 
     def play
-      @stdin.puts "LOAD #{pick_a_song}" 
+      #@stdin.puts "LOAD #{pick_a_song}"
+      play_music_until_stopped
+      Thread.new do
+        while playing?
+          puts @stdout.readlines
+          puts @stderr.readlines
+          sleep 1
+        end
+      end
     end
-    
+
     def stop
-      stdin.puts 'STOP' 
+      stdin.puts 'STOP'
     end
 
     #######
     private
     #######
-    
+
     def playing?
       @music_thread and @music_thread.alive?
     end
 
     def play_music_until_stopped
-      while true
-        play_next_song
+      Thread.new do
+        while true
+          play_next_song
+        end
       end
     end
 
