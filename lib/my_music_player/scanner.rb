7552f02135700a99ae18089661b5d46f2c129049
Jonathan Garvin <jon@5valleys.com>
2013-08-25 16:59:34 -0600

    desingletonize scanner

--- lib/my_music_player/scanner.rb
+++ lib/my_music_player/scanner.rb
@@ -1,10 +1,9 @@
 module MyMusicPlayer
   class Scanner
-    include Singleton
 
     def ls
       Array.new.tap do |files|
-        Find.find(music_path) do |file| 
+        Find.find(music_path) do |file|
           next if File.directory?(file)
           files << relative_path(file)
         end
@@ -14,13 +13,13 @@ module MyMusicPlayer
     #######
     private
     #######
-    
+
     def music_path
       Configuration.instance.music_path
     end
 
     def relative_path(absolute_path)
-      absolute_path.gsub(/#{music_path}\//,'') 
+      absolute_path.gsub(/#{music_path}\//,'')
     end
 
   end
--- test/unit/scanner_test.rb
+++ test/unit/scanner_test.rb
@@ -4,9 +4,10 @@ module MyMusicPlayer
   class ScannerTest < MiniTest::Unit::TestCase
 
     def test_should_find_files_in_music_folder
-      assert Scanner.instance.ls.include?('point1sec.mp3')
-      assert Scanner.instance.ls.include?('subfolder/insubfolder.mp3')
+      files = Scanner.new.ls
+      assert files.include?('point1sec.mp3')
+      assert files.include?('subfolder/insubfolder.mp3')
     end
-    
+
   end
 end
