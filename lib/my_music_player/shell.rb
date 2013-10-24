997bcb52d2506f8850aaaee6ae8336a1814dd09e
Jonathan Garvin <jon@5valleys.com>
2013-10-23 18:34:54 -0600

    convert shell commands to lambdas

--- lib/my_music_player/shell.rb
+++ lib/my_music_player/shell.rb
@@ -1,39 +1,36 @@
 module MyMusicPlayer
   class Shell
 
+    COMMANDS = {
+      exit: ->() { player.stop; player.quit; puts 'Exiting...'; },
+      info: ->() { print_info }
+    }.with_indifferent_access
+
     def run(*commands)
       commands << :exit unless commands.empty?
       command = nil
       until command.to_s == 'exit'
         print 'mmp> '
         command = commands.shift || gets.strip
-        if self.respond_to?("_mmp_#{command}")
-          send("_mmp_#{command}")
+        if player.respond_to?(command)
+          player.send(command)
+        elsif COMMANDS.has_key?(command)
+          COMMANDS[command].call
         else
           puts "Unrecognized Command: #{command}"
         end
       end
     end
 
-    def _mmp_exit
-      player.stop
-      player.quit
-      puts "Exiting..."
+    #######
+    private
+    #######
+
+    def self.player
+      @player ||= Player.new
     end
 
-    def _mmp_play
-      player.play
-    end
-
-    def _mmp_pause
-      player.pause
-    end
-
-    def _mmp_stop
-      player.stop
-    end
-
-    def _mmp_info
+    def self.print_info
       puts player.current_track.full_path
       puts player.current_track.title
       puts player.current_track.artist.name
@@ -42,12 +39,8 @@ module MyMusicPlayer
       puts "Seconds: #{player.seconds} (#{player.seconds_remaining})"
     end
 
-    #######
-    private
-    #######
-
     def player
-      @player ||= Player.new
+      self.class.player
     end
   end
 end
--- test/unit/shell_test.rb
+++ test/unit/shell_test.rb
@@ -4,48 +4,43 @@ module MyMusicPlayer
   class ShellTest < MiniTest::Unit::TestCase
     attr_reader :mock_player, :mock_scanner
 
-    def setup
-      @mock_player = mock('player')
-      @mock_player.expects(:stop)
-      @mock_player.expects(:quit)
-      Player.stubs(:new).returns(@mock_player)
-    end
-
-    def test_execute_a_command_and_exit
-      Shell.any_instance.expects(:_mmp_foobar)
-      assert_equal('',$stdout.string)
-      Shell.new.run(:foobar)
-      assert_match(/Exiting/,$stdout.string)
-    end
-
-    def test_gracefully_fail_to_execute_command
-      assert_equal('',$stdout.string)
-      Shell.new.run(:ping)
-      assert_match(/Unrecognized Command: ping/,$stdout.string)
-    end
-
-    describe 'info' do
-      let(:track) { create(:track) }
-      let(:player) { Player.new }
+    describe Shell do
+      let(:mock_player) { mock('player') }
+      let(:shell) { Shell.new }
 
       before do
-        Shell.any_instance.stubs(:player => player)
-        player.stubs(:current_track => track)
+        mock_player.stubs(:stop)
+        mock_player.stubs(:quit)
+        Shell.stubs(:player).returns(mock_player)
       end
 
-      def test_execute_info
+      def test_gracefully_fail_to_execute_command
         assert_equal('',$stdout.string)
-        Shell.new.run(:info)
-        refute_equal('',$stdout.string)
+        shell.run(:ping)
+        assert_match(/Unrecognized Command: ping/,$stdout.string)
       end
-    end
 
-    [:play,:pause,:stop].each do |command_name|
-      define_method("test_execute_#{command_name}") do
-        mock_player.expects(command_name)
-        Shell.new.run(command_name)
+      describe 'info' do
+        before do
+          Shell.expects(:print_info)
+        end
+
+        def test_execute_info
+          assert_equal('',$stdout.string)
+          shell.run(:info)
+          refute_equal('',$stdout.string)
+        end
       end
-    end
 
+      describe 'player commands' do
+        def test_execute_player_commands
+          [:play,:stop,:quit].each do |command|
+            mock_player.expects(command)
+            shell.run(command)
+          end
+        end
+      end
+
+    end
   end
 end
